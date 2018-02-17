beb6920c07c2363d798db6bdd5c582741d8de956
bader <bader.chtara@esprit.tn>
2018-02-17 11:06:16 +0100

    bundle stat+ bundle RS

--- src/ObjetBundle/Controller/ObjetController.php
+++ src/ObjetBundle/Controller/ObjetController.php
@@ -2,6 +2,7 @@
 
 namespace ObjetBundle\Controller;
 
+use Ob\HighchartsBundle\Highcharts\Highchart;
 use ObjetBundle\Entity\Objet;
 use ObjetBundle\Form\ObjetType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,40 +11,82 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class ObjetController extends Controller
 {
-    public function ajoutobjAction(Request $request,UserInterface $username)
+    public function ajoutobjtrouvAction(Request $request, UserInterface $username)
     {
-        $objet=new Objet();
-        $form=$this->createForm(ObjetType::class,$objet);
+        $objet = new Objet();
+        $form = $this->createForm(ObjetType::class, $objet);
         $form->handleRequest($request);
         if ($form->isValid() && $form->isSubmitted()) {
             $file = $objet->getPhoto();
-            $fileName = md5(uniqid()).'.'.$file->guessExtension();
-            $path = "C:/xampp/htdocs/pidev2/web" ;
+            $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+            $path = "C:/xampp/htdocs/pidev2/web";
             $file->move(
                 $path,
                 $fileName
             );
             $username = $this->container->get('security.token_storage')->getToken()->getUser();
+            $objet->setNature('Objet Trouvé');
             $objet->setUser($username);
             $objet->setPhoto($fileName);
             $objet = $form->getData();
             $em = $this->getDoctrine()->getManager();
             $em->persist($objet);
             $em->flush();
+            return $this->redirectToRoute("affichobjtrouv");
         }
 
 
-        return $this->render('ObjetBundle:Objet:ajoutobj.html.twig', array('form'=>$form->createView()
+        return $this->render('ObjetBundle:Objet:ajoutobj.html.twig', array('form' => $form->createView()
             // ...
         ));
     }
 
-    public function affichobjAction()
+    public function ajoutobjperdAction(Request $request, UserInterface $username)
     {
-        $em =$this->getDoctrine()->getManager();
-        $objet=$em->getRepository(Objet::class)->objtrouv();
+        $objet = new Objet();
+        $form = $this->createForm(ObjetType::class, $objet);
+        $form->handleRequest($request);
+        if ($form->isValid() && $form->isSubmitted()) {
+            $file = $objet->getPhoto();
+            $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+            $path = "C:/xampp/htdocs/pidev2/web";
+            $file->move(
+                $path,
+                $fileName
+            );
+            $username = $this->container->get('security.token_storage')->getToken()->getUser();
+            $objet->setNature('Objet Perdu');
+            $objet->setUser($username);
+            $objet->setPhoto($fileName);
+            $objet = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($objet);
+            $em->flush();
+            return $this->redirectToRoute("affichobjperd");
+        }
 
-        return $this->render('ObjetBundle:Objet:affichobj.html.twig', array('objet'=>$objet
+
+        return $this->render('ObjetBundle:Objet:ajoutobj.html.twig', array('form' => $form->createView()
+            // ...
+        ));
+    }
+
+    public function affichobjtrouvAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $objet = $em->getRepository(Objet::class)->objtrouv();
+
+        return $this->render('ObjetBundle:Objet:affichobj.html.twig', array('objet' => $objet, 'nature' => 1
+            // ...
+        ));
+    }
+
+    public function affichobjperdAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $objet = $em->getRepository(Objet::class)->objperd();
+
+        return $this->render('ObjetBundle:Objet:affichobj.html.twig', array('objet' => $objet, 'nature' => 2
             // ...
         ));
     }
@@ -51,28 +94,88 @@ class ObjetController extends Controller
 
     public function deleteobjAction($id)
     {
-        $em=$this->getDoctrine()->getManager();
-        $objet=$em->getRepository(Objet::class)->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $objet = $em->getRepository(Objet::class)->find($id);
         $em->remove($objet);
         $em->flush();
-        return $this->redirectToRoute("affichobj");
+        if ($objet->getNature() == 'Objet Perdu') {
+            return $this->redirectToRoute("affichobjperd");
+        } else {
+            return $this->redirectToRoute("affichobjtrouv");
+        }
     }
 
-    public function updateobjAction(Request $request,$id)
+    public function updateobjAction(Request $request, $id)
     {
-        $em=$this->getDoctrine()->getManager();
-        $objet=$em->getRepository(Objet::class)->find($id);
-        $Form = $this->createForm( ObjetType::class, $objet);
+        $em = $this->getDoctrine()->getManager();
+        $objet = $em->getRepository(Objet::class)->find($id);
+        $Form = $this->createForm(ObjetType::class, $objet);
         $Form->handleRequest($request);
-        if ($Form->isValid() && $Form->isSubmitted()){
+        if ($Form->isValid() && $Form->isSubmitted()) {
+            $file = $objet->getPhoto();
+            $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+            $path = "C:/xampp/htdocs/pidev2/web";
+            $file->move(
+                $path,
+                $fileName
+            );
+            $username = $this->container->get('security.token_storage')->getToken()->getUser();
+            $objet->setUser($username);
+            $objet->setPhoto($fileName);
             $em->persist($objet);
             $em->flush();
+            if ($objet->getNature() == 'Objet Perdu') {
+                return $this->redirectToRoute("affichobjperd");
+            } else {
+                return $this->redirectToRoute("affichobjtrouv");
+            }
 
         }
-        return $this->render('ObjetBundle:Objet:ajoutobj.html.twig', array('form'=>$Form->createView()
-            // ...
+        return $this->render('ObjetBundle:Objet:ajoutobj.html.twig', array('form' => $Form->createView()            // ...
         ));
 
     }
 
-}
+    public function objetAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $objets = $em->getRepository(Objet::class)->findAll();
+       /* $objetsper = $em->getRepository(Objet::class)->nbobjperd();
+        //$objetstrouv = $em->getRepository(Objet::class)->nbobjtrouv();
+        $tab = array();
+        $categories = array();
+        $tab1=array('ObjetType'=>$objets->getNature(),'Nombre'=>$objetsper)
+        foreach ($tab1 as $tb)
+        {
+            array_push($tab, $tab1);
+            array_push($categories, $objet->getNature());
+        }
+        // Chart
+        $series = array( array("name" => "Les Objets Perdus", "data" => $tab) );
+        $ob = new Highchart();
+        $ob->chart->renderTo('linechart');
+        #id du div où afficher le graphe
+        $ob->title->text('Objet');
+        $ob->xAxis->title(array('text' => "Date"));
+        $ob->yAxis->title(array('text' => "Nature"));
+        $ob->xAxis->categories($categories);
+        $ob->series($series);*/
+        return $this->render('ObjetBundle:Objet:Objet.html.twig', array('chart' => $ob));
+
+    }
+
+    public function coinobjperdAction()
+    {
+
+        return $this->render('ObjetBundle:Objet:coinobjperd.html.twig');
+    }
+
+    public function coinobjtrouvAction()
+    {
+
+        return $this->render('ObjetBundle:Objet:coinobjtrouv.html.twig');
+    }
+
+
+
+    }
